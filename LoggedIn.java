3e9a3b0fa0aa7c2eea5e815890790e8e83227cb2
amna-tiwana <57238668+amna-tiwana@users.noreply.github.com>
2020-07-08 17:08:53 -0700

    Add files via upload
    This is the LoggedIn class.

---
+++ LoggedIn.java
@@ -0,0 +1,81 @@
+
+public class LoggedIn {
+	private String chat_log;
+	private String contacts;
+	private boolean read;
+	private boolean backChatlog;
+	private String add_Friend;
+	private String searchUser;
+	private String addUser;
+	private String removeUser;
+	
+	public String getChat_log()
+	{
+		return chat_log;
+	}
+	public String getContacts()
+	{
+		return contacts;
+	}
+	public boolean getRead()
+	{
+		return read; 
+	}
+	public void setBackChatlog()
+	{
+		backChatlog = backChatlog;
+	}
+	public boolean getBackChatlog()
+	{
+		return false;
+	}
+	public void setAdd_Friend()
+	{
+		add_Friend = add_Friend;
+	}
+	public String getAdd_Friend()
+	{
+		return add_Friend;
+	}
+	public void setSearchUser()
+	{
+		searchUser = searchUser;
+	}
+	public String getSearchUser()
+	{
+		return searchUser;
+	}
+	public void setAddUser()
+	{
+		addUser = addUser;
+	}
+	public String getAddUser()
+	{
+		return addUser;
+	}
+	public void setRemoveUser()
+	{
+		removeUser = removeUser;
+	}
+	public String getRemoveUser()
+	{
+		return removeUser;
+	}
+	public void setStartChat(String startChat)
+	{
+		startChat = startChat;
+	}
+	public String getStartChat(String chat)
+	{
+		return chat;
+	}
+	public void setChangePassword(String changePassword)
+	{
+		changePassword = changePassword;
+	}
+	public String getChangePassword(String password)
+	{
+		return password;
+	}
+
+}
